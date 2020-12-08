90e32538cac41fa9ff2e753525b11020ad941c3e
Romain <axilais.romain@gmail.com>
2020-12-08 15:42:47 +0100

    Debut mini projet

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ let app = express();
 let port = 3000;
 
 // Préparation du serveur
+
+app.use(express.static(__dirname + '/www'));
+
+//Ajout des librairies
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
 app.listen(port, () => {
     console.log('Le serveur est en route');
     console.log(`Serveur listening at http://localhost:${port}`);
 })
 
-app.use(express.static(__dirname + '/www'));
-
 // Redirection vers page index
 app.get('/', (req, res, next) => {
-    res.sendFile('/www/index.html');
+    res.sendFile('www/index.html');
 });
