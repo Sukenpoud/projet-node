94637eff223e0751613955328e0e8a6ff101985e
Romain <axilais.romain@gmail.com>
2020-12-08 15:34:17 +0100

    Ajout page accueil

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ let express = require('express');
 let app = express();
 let port = 3000;
 
+// Préparation du serveur
 app.listen(port, () => {
     console.log('Le serveur est en route');
     console.log(`Serveur listening at http://localhost:${port}`);
 })
 
+app.use(express.static(__dirname + '/www'));
+
+// Redirection vers page index
 app.get('/', (req, res, next) => {
-    res.send('Bonjour !');
+    res.sendFile('/www/index.html');
 });
